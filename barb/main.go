d9f1d159328f1c49ca5dfe804b53d62fa9a89ec1
Erik Hollensbe <github@hollensbe.org>
2017-01-14 01:24:36 -0800

    blah
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- barb/main.go
+++ barb/main.go
@@ -22,66 +22,59 @@ func main() {
 			Action:      reply,
 		},
 		{
-			Name:        "prs",
-			Description: "Manipulate PRs",
-			Usage:       "Manipulate PRs",
-			Subcommands: []cli.Command{
-				{
-					Name:        "list",
-					Description: "List PRs",
-					Usage:       "List PRs",
-					Action:      listPRs,
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name:  "s, state",
-							Usage: "State of prs (open, closed)",
-							Value: "open",
-						},
-						cli.StringFlag{
-							Name:  "b, sort-by",
-							Usage: "Sort by this value",
-							Value: "created",
-						},
-						cli.StringFlag{
-							Name:  "d, direction",
-							Usage: "Direction of sort",
-							Value: "desc",
-						},
-						cli.IntFlag{
-							Name:  "m, max-pages",
-							Usage: "Maximum number of list pages to fetch",
-							Value: 5,
-						},
-					},
+			Name:        "list",
+			Description: "List PRs",
+			Usage:       "List PRs",
+			Action:      listPRs,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "s, state",
+					Usage: "State of prs (open, closed)",
+					Value: "open",
 				},
-				{
-					Name:        "create",
-					Description: "Create a PR",
-					Usage:       "Create a PR",
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name: "t, title",
-						},
-						cli.StringFlag{
-							Name:  "b, base",
-							Value: "master",
-						},
-					},
-					Action: createPR,
+				cli.StringFlag{
+					Name:  "b, sort-by",
+					Usage: "Sort by this value",
+					Value: "created",
 				},
-				{
-					Name:        "merge",
-					Description: "Merge a PR",
-					Usage:       "Merge a PR",
-					Action:      mergePR,
+				cli.StringFlag{
+					Name:  "d, direction",
+					Usage: "Direction of sort",
+					Value: "desc",
 				},
-				{
-					Name:        "diff",
-					Description: "Get the diff for a PR",
-					Usage:       "Get the diff for a PR",
-					Action:      diffPR,
+				cli.IntFlag{
+					Name:  "m, max-pages",
+					Usage: "Maximum number of list pages to fetch",
+					Value: 5,
 				},
 			},
+		},
+		{
+			Name:        "create",
+			Description: "Create a PR",
+			Usage:       "Create a PR",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "t, title",
+				},
+				cli.StringFlag{
+					Name:  "b, base",
+					Value: "master",
+				},
+			},
+			Action: createPR,
+		},
+		{
+			Name:        "merge",
+			Description: "Merge a PR",
+			Usage:       "Merge a PR",
+			Action:      mergePR,
+		},
+		{
+			Name:        "diff",
+			Description: "Get the diff for a PR",
+			Usage:       "Get the diff for a PR",
+			Action:      diffPR,
 		},
 	}
 
